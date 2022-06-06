2f2eddb88a3e1847c376fb6309f546df2d94c62b
KimberlyRosaly <01010110.v@gmail.com>
2022-06-05 23:56:31 -0400

    alter the App component to handle some React client side routing with basic nested paths for logging in and signing up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import { Routes, Route, Outlet } from 'react-router-dom'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+export default function App() {
+  
+  return (<>
+
+
+    {/*  ==================================================================== */}
+    {/*                                             CLIENT SIDE R O U T I N G */}
+    <Routes>      
+        {/* ====================================================== ROOT PATH  */}
+        <Route path="/" element={} />
+        {/* =============================================site.com/access-portal/  */}
+        <Route path="/access-portal" element={} >
+          {/* ===========================================site.com/access-portal/login/  */}
+          <Route path="/login" element={} />
+          {/* ===========================================site.com/access-portal/signup/  */}
+          <Route path="/signup" element={} />
+        </Route>
+    </Routes>   
+    {/*  ==================================================================== */}    
+    
+
+  </>);
 }
-
-export default App;
