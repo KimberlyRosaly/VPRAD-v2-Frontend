1b712238b7ae11db84aa3ed6ede1646698915ac3
KimberlyRosaly <01010110.v@gmail.com>
2022-06-07 20:08:49 -0400

    create container component to handle logging and signing up shenanigans

---
+++ src/containers/Portal.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+export default function Portal() {  
+
+
+    return (
+        <>
+            <div>PORTAL CONTAINER COMPONENT</div>
+        </>
+    )
+        
+
+    
+    }
