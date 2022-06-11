9d71891566ea84b60abdcd8bb07de1858b30c1e8
KimberlyRosaly <01010110.v@gmail.com>
2022-06-10 22:59:40 -0400

    component to mount information about a building

---
+++ src/components/Building.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+export default function Building() {  
+
+
+    return (
+        <>
+            <div>BUILDING DATA COMPONENT</div>
+        </>
+    )
+        
+
+    
+    }
