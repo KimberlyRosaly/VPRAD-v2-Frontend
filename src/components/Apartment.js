324b46e210e9dad1deaed7424091530966619591
KimberlyRosaly <01010110.v@gmail.com>
2022-06-10 22:59:26 -0400

    component will house data about an individual apartment

---
+++ src/components/Apartment.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+export default function Apartment() {  
+
+
+    return (
+        <>
+            <div>APARTMENT DATA COMPONENT</div>
+        </>
+    )
+        
+
+    
+    }
