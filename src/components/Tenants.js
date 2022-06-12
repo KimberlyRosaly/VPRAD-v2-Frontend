b340243927d45ca8aa30e432ed62ad27dc77462f
KimberlyRosaly <01010110.v@gmail.com>
2022-06-11 21:57:16 -0400

    component to handle multiple iterations of tenants

---
+++ src/components/Tenants.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+export default function Tenants() {  
+
+
+    return (
+        <>
+            <div>TENANTS DATA COMPONENT</div>
+        </>
+    )
+        
+
+    
+    }
