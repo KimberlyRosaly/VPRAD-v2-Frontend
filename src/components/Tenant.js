ef44301842520d3bf36fce8153797c362d8143f5
KimberlyRosaly <01010110.v@gmail.com>
2022-06-11 21:57:52 -0400

    component will display data about a single tenant

---
+++ src/components/Tenant.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+export default function Tenant() {  
+
+
+    return (
+        <>
+            <div>SINGLE TENANT DATA COMPONENT</div>
+        </>
+    )
+        
+
+    
+    }
