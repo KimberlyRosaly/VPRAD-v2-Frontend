da95f800275a2499d80ca22b7c0451ce5609ccee
KimberlyRosaly <01010110.v@gmail.com>
2022-06-12 23:13:08 -0400

    add some new routes - URLs with their corresponding component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './containers/Home';
 import Portal from './containers/Portal';
 import Login from './components/Login';
+import Signup from './components/Signup';
+import Buildings from './components/Buildings';
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 
@@ -19,15 +22,25 @@ export default function App() {
     {/*  ==================================================================== */}
     {/*                                             CLIENT SIDE R O U T I N G */}
     <Routes>      
+
         {/* ====================================================== ROOT PATH  */}
         <Route path="/" element={<Home />} />
-        {/* =============================================site.com/access-portal/  */}
+
+        {/* ===================================================site.com/access-portal/  */}
         <Route path="/access-portal" element={<Portal />} >
           {/* ===========================================site.com/access-portal/login/  */}
           <Route path="/login" element={<Login />} />
           {/* ===========================================site.com/access-portal/signup/  */}
           <Route path="/signup" element={<Signup />} />
         </Route>
+
+        {/* ====================================================== site.com/dashboard/  */}
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* ====================================================== site.com/buildings/  */}
+        <Route path="/buildings" element={<Buildings />} />
+
+
     </Routes>   
     {/*  ==================================================================== */}    
     
