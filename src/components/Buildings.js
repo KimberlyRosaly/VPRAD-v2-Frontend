c8e4591446f80bfbf82ba05051db2d790b0c4cf1
KimberlyRosaly <01010110.v@gmail.com>
2022-06-12 23:12:36 -0400

    index directory of buildings in complex

---
+++ src/components/Buildings.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+export default function Buildings() {  
+
+
+    return (
+        <>
+            <div>BUILDINGS DATA COMPONENT</div>
+        </>
+    )
+        
+
+    
+    }
