c071949a30ab4e6e0513b5b75465e0e569497cad
KimberlyRosaly <01010110.v@gmail.com>
2022-06-14 23:20:20 -0400

    component to house data about all apartments - index page

---
+++ src/components/Apartments.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+export default function Apartments() {  
+
+
+    return (
+        <>
+            <div>APARTMENTS INDEX DATA COMPONENT</div>
+        </>
+    )
+        
+
+    
+    }
