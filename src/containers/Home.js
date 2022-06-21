a3e1831118569adc6f268b5068a9b5f9665c75db
KimberlyRosaly <01010110.v@gmail.com>
2022-06-20 20:00:28 -0400

    change buttons into react router client side routing links, add a footer with more application details

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,20 +1,44 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import { Link } from 'react-router-dom'
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 
+// H O M E   C O N T A I N E R   C O M P O N E N T
 export default function Home() {  
 
 
     return (
         
-            <div className='home-container'>HOME CONTAINER COMPONENT
-
+            <div className='home-container'>
+                <hr />
                 <h1>VEHICLE PERMITS</h1>
                 <h2>REGISTRATION & DATABASE</h2>
+                <hr />
 
-                <button>LOG IN</button>
+                <Link to={'/access-portal/login'}>
+                    <button>
+                        LOG IN
+                    </button>
+                </Link>
+
+                <Link to={'/access-portal/signup'}>
+                    <button>
+                        SIGN UP
+                    </button>
+                </Link>
+                <hr />
+
+                <footer>
+                    <h5>The Name'O Apartment Complex</h5>
+                    <h6>
+                        123 South Avenue Street
+                        <br />
+                        Towningham, New York 12398
+                    </h6>
+                </footer>
                 
-                <button>SIGN UP</button>
             </div>
         
     )
