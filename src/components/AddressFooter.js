4bb402152f9202774bae7d1c6af47a20d15a33ed
KimberlyRosaly <01010110.v@gmail.com>
2022-06-21 21:54:20 -0400

    presentation component to display data : apartment complex address

---
+++ src/components/AddressFooter.js
@@ -0,0 +1,27 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import React from 'react'
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+// D I S P L A Y   A D D R E S S   PRESENTATIONAL COMPONENT
+export default function AddressFooter() {  
+
+
+    return (
+        
+            <div>
+              <h5>The Name'O Apartment Complex</h5>
+                    <h6>
+                        123 South Avenue Street
+                        <br />
+                        Towningham, New York 12398
+                    </h6>
+            </div>        
+    )
+        
+
+    
+    }    
+    
+    
+    
